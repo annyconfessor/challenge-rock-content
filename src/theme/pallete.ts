074103701cb825851953d8233d24d0aa20eddfbc
Anny Confessor <anny.confessor@gmail.com>
2021-11-19 16:19:12 -0300

    fix: change transparency pallete color

--- src/theme/pallete.ts
+++ src/theme/pallete.ts
@@ -1,14 +1,8 @@
 const pallete = {
     white: '#fff',
-    green: {
-      default: '#19C0A0',
-      transparency: '#CDEFEC',
-      light: '#44DABD',
-      dark: '#16A086'
-    },
     blue: {
       default: '#4166AF',
-      transparency: '#b8cde0',
+      transparency: '#bacee0',
       light: '#64B5F6',
       dark: '#395387'
     },
@@ -29,9 +23,6 @@ const pallete = {
       transparency: '#F5F5F5',
       light: '#E1E2E7',
       dark: '#575B64',
-      300: '#C5CCD6',
-      600: '#3A404D',
-      700: '#161D27'
     }
   }
   
