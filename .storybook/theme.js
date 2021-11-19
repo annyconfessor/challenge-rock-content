0a959500ec41ab6ff6af151659796a5df05bf5d3
Anny Confessor <anny.confessor@gmail.com>
2021-11-19 16:45:32 -0300

    fix: texts fixed

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,6 +2,6 @@ import { create } from '@storybook/theming'
 
 export const baseTheme = create({
   base: 'light',
-  brandTitle: 'Rocket Content Chanllenge',
+  brandTitle: 'Rock Content Chanllenge',
   brandImage: 'https://conteudo.precocerto.co/wp-content/uploads/2015/05/roquecontente.png'
 })
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Challenge Rocket Content
+# Challenge Rock Content
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
